ac1c7f940454949a0f4383539497fda3ce530e21
Rushnitha <rushnithasirsilla@gmail.com>
2026-03-27 08:39:24 +0530

    Restore remaining files

---
+++ hh.py
@@ -0,0 +1,25 @@
+users = [
+    {"id": 0, "name": "Hero"},
+    {"id": 1, "name": "Dunn"},
+    {"id": 2, "name": "Sue"},
+    {"id": 3, "name": "Chi"},
+    {"id": 4, "name": "Thor"},
+    {"id": 5, "name": "Clive"},
+    {"id": 6, "name": "Hicks"},
+    {"id": 7, "name": "Devin"},
+    {"id": 8, "name": "Kate"},
+    {"id": 9, "name": "Klein"}
+]
+friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5),
+                    (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+
+def number_of_friends(user):
+    """How many friends does _user_ have?"""
+    user_id = user["id"]
+    friend_ids = friendships[user_id]
+    return len(friend_ids)
+print(number_of_friends(users[0]))
+print(number_of_friends(users[3]))
+total_connections = sum(number_of_friends(user) for user in users)
+print(total_connections)
